54797f0cb9dc84e1968fadfb3d20f4b64ec66801
guillaume <guillaume@takewalks.com>
2019-02-01 13:32:08 +0000

    init

---
+++ layouts/default.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+html, body {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  background: #222;
+  color: white;
+  width: 100vw;
+  height: 100vh;
+}
+
+a, a:visited {
+  color: #3edada;
+  text-decoration: none;
+}
+
+.container {
+  padding: 60px;
+  width: 100vw;
+  height: 100vh;
+  background: #444;
+}
+
+.page-enter-active {
+  animation: acrossIn .45s ease-out both;
+} 
+
+.page-leave-active {
+  animation: acrossOut .65s ease-in both;
+} 
+
+@keyframes acrossIn {
+  0% {
+    transform: translate3d(-100%, 0, 0);
+  }
+  100% {
+    transform: translate3d(0, 0, 0);
+  }
+}
+
+@keyframes acrossOut {
+  0% {
+    transform: translate3d(0, 0, 0);
+  }
+  100% {
+    transform: translate3d(100%, 0, 0);
+  }
+}
+</style>
+
