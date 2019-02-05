9752ad9faabc4cf30b54b08a34fa89ce6bec9d37
guillaume <guillaume@takewalks.com>
2019-02-05 19:01:35 +0000

    read routes from config file

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="container">
+  <div class="container-fluid">
     <nav class="navbar" role="navigation" aria-label="main navigation">
      
       <div class="navbar-brand">
-        <a class="navbar-item" href="/">Portfolio</a>
+        <a class="navbar-item" href="/">Project</a>
         <button class="button navbar-burger">
           <span></span>
           <span></span>
@@ -12,11 +12,25 @@
       </div>
       <div class="navbar-menu">
         <div class="navbar-end">
-          <nuxt-link class="navbar-item" to="/">Home</nuxt-link>
+          <template v-for="route in routes">
+            <nuxt-link class="navbar-item" :key="route.name" :to="route.path">{{ route.name }}</nuxt-link>
+          </template>
+          <!-- <nuxt-link class="navbar-item" to="/">Home</nuxt-link>
           <nuxt-link class="navbar-item" to="/product">Products</nuxt-link>
-          <nuxt-link class="navbar-item" to="/contact">Contact</nuxt-link>
+          <nuxt-link class="navbar-item" to="/contact">Contact</nuxt-link> -->
         </div>
       </div>
     </nav>
   </div>
 </template>
+
+<script>
+import { routes } from '@/static/route.js'
+export default {
+  data () {
+    return {
+      routes: routes
+    }
+  }
+}
+</script>
