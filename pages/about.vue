a3e4b3ee8bb53bcd27b46c7c35b9d3c508319a76
guillaume <guillaume@takewalks.com>
2019-02-05 19:10:17 +0000

    add build

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <h1>This is the ABOUT page</h1>
+    <p>
+      lorem sdomsdf sdfj ksd fksd fksd fkj dsf 
+    </p>
+  </div>
+</template>
+
--- static/route.js
+++ static/route.js
@@ -1,5 +1,6 @@
 export const routes = [
   { name: "Home", path: '/' },
   { name: "Products 1", path: '/product' },
-  { name: "contacts 1", path: '/contact' }
+  { name: "contacts 1", path: '/contact' },
+  { name: "about 1", path: '/about' }
 ]
