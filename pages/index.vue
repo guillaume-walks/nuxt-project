dde6af1931ee3eede536f7125410a5d3bf168811
Guillaume Ader <guillaume@Guillaumes-MacBook-Pro-2.local>
2019-02-19 20:04:37 +0000

    add build

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="container">
-    <h1>Welcome! App</h1>
-    <p><nuxt-link to="/product">Product page</nuxt-link></p>
-    <p><nuxt-link to="/contact">contact page</nuxt-link></p>
+    <h1 class="toto">Welcome! App</h1>
+    <p>
+      <nuxt-link to="/product">Product page</nuxt-link>
+    </p>
+    <p>
+      <nuxt-link to="/contact">contact page</nuxt-link>
+    </p>
   </div>
 </template>
 
 <style>
+@import "~/assets/style.css";
 .container {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   padding: 60px;
 }
 </style>
