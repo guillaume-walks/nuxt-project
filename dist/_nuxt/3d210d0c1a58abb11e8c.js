45fd2b0a7c832a5efdd2b3f6aec3313229fac434
guillaume <guillaume@takewalks.com>
2019-03-04 14:31:42 +0000

    add build

---
+++ dist/_nuxt/3d210d0c1a58abb11e8c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{180:function(t,n,c){"use strict";c.r(n);var e=c(9),component=Object(e.a)({},function(){var t=this.$createElement,n=this._self._c||t;return n("div",{staticClass:"container"},[n("h1",{},[this._v("Welcome! App")]),this._v(" "),n("p",[n("nuxt-link",{attrs:{to:"/product/"}},[this._v("Product page")])],1),this._v(" "),n("p",[n("nuxt-link",{attrs:{to:"/contact"}},[this._v("contact page")])],1)])},[],!1,null,null,null);n.default=component.exports}}]);
+//# sourceMappingURL=3d210d0c1a58abb11e8c.js.map
