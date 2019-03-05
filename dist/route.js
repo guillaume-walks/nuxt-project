2f1c7bf73f34bcf798f9503960555756033c6b7b
Guillaume Ader <guillaume@Guillaumes-MacBook-Pro-2.local>
2019-03-05 20:05:09 +0000

    remove dist from tracking

--- dist/route.js
+++
@@ -1,6 +0,0 @@
-export const routes = [
-  { name: "Home", path: '/' },
-  { name: "Products 1", path: '/product' },
-  { name: "contacts 1", path: '/contact' },
-  { name: "about 1", path: '/about' }
-]
