e4ca05e3297bec2c48d890ecc527ccb0588ed57d
Guillaume Ader <guillaume@Guillaumes-MacBook-Pro-2.local>
2019-03-05 22:36:49 +0000

    add build

---
+++ dist/route.js
@@ -0,0 +1,7 @@
+export const routes = [
+  { name: "Home", path: '/' },
+  { name: "Products", path: '/product' },
+  { name: "Gallery", path: '/gallery' },
+  { name: "contacts", path: '/contact' },
+  { name: "about", path: '/about' }
+]
