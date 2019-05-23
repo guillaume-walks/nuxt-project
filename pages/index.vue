f73a8b2035d00ffd2c40d455d8037583c86f4962
guillaume <guillaume@takewalks.com>
2019-05-23 10:18:26 +0100

    start building response with every dependency

--- pages/index.vue
+++ pages/index.vue
@@ -17,17 +17,54 @@
 <script>
 import * as Components from "~/components/utils/importAll";
 import {createClient} from '~/plugins/contentful.js'
+import { async } from 'q';
 
 const client = createClient()
+
+const test = () => {
+  console.log(test)
+}
+async function buildPage (uid) {
+  const page = await getEntry(uid)
+
+  console.log(page)
+  const request = buildStack(page, [])
+  console.log(request)
+}
+const buildStack = (obj, list) => {
+  for (var i in obj) {
+    console.log(i, obj[i])
+    if (i === 'sys') {
+      list = list.concat(obj[i].id)
+    } else {
+      for (var j in obj[i]) {
+        if (Array.isArray(obj[i][j])) {
+          buildStack(obj[i][j], list)
+        }
+      }      
+    }
+  }
+  return list
+}
+async function getEntry (uid) {
+  const promise = new Promise((resolve, reject) => {
+    client.getEntry(uid)
+      .then(entry => resolve(entry))
+      .catch(err => resolve(err));
+  })   
+  return promise   
+}
 
 export default {
   components: {
     ...Components
   },
-  asyncData({env}) {
-      client.getEntry('34MlmiuMgU8wKCOOIkAuMy')
-        .then(entry => console.log(entry))
-        .catch(err => console.log(err));
+  asyncData({env}) {  
+    console.log(test)
+    buildPage('3kw0QgZtzCFw3P2MiYwpIP')
+      // client.getEntry('3kw0QgZtzCFw3P2MiYwpIP')
+      //   .then(entry => console.log(entry))
+      //   .catch(err => console.log(err));
   }
 };
 </script>
