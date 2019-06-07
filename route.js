0cba6371d01f48051c75f40de717a6b6f58213b5
guillaume <guillaume@takewalks.com>
2019-06-07 10:52:03 +0100

    530cda1

---
+++ route.js
@@ -0,0 +1,9 @@
+export const routes = [
+  { name: "Home", path: '/' },
+  { name: "Products", path: '/product' },
+  { name: "Gallery", path: '/gallery' },
+  { name: "contacts", path: '/contact' },
+  { name: "about", path: '/about' },
+  { name: "Dynamic", path: '/dynamic' },
+  { name: 'contenful', path: '/contenful', uid: '3kw0QgZtzCFw3P2MiYwpIP' }
+]
